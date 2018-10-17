e9ec9d1a7c93bbc3dbc2d372a4cf9f052bd7dc58
chantezan <pablo.poblete@ing.uchile.cl>
2018-10-16 21:08:43 -0300

    - se agregan casas a etapas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
         var ETAPA = 1;
         var CANTIDAD_JUEGOS = 2;
         var timeoutHandle;
-        var size = 100;
+        var size = 70;
         var canvas = document.getElementById('myCanvas');
         var elemLeft = $("#myCanvas").offset().left;
         var elemTop = $("#myCanvas").offset().top;
@@ -93,8 +93,9 @@
         var tiempo_restante = TIEMPO_JUEGO;
         var lineas = [];
         var lineas_antiguas = [];
-        juego1 = [{x:300,y:260,selected:false},{x:400,y:600,selected:false},{x:250,y:600,selected:false},{x:690,y:400,selected:false}];
-        juego2 = [{x:400,y:260,selected:false},{x:400,y:500,selected:false},{x:250,y:600,selected:false},{x:690,y:400,selected:false},{x:690,y:600,selected:false}];
+        juego1 = [{x:280,y:460,selected:false},{x:520,y:460,selected:false},{x:780,y:600,selected:false},{x:470,y:240,selected:false},{x:800,y:240,selected:false},{x:400,y:300,selected:false},{x:400,y:600,selected:false},{x:250,y:600,selected:false},{x:690,y:400,selected:false}];
+        juego2 = [{x:180,y:310,selected:false},{x:200,y:600,selected:false},{x:240,y:240,selected:false},{x:280,y:700,selected:false},{x:300,y:390,selected:false},{x:400,y:280,selected:false},{x:500,y:410,selected:false},{x:570,y:600,selected:false},{x:150,y:450,selected:false}
+        ,{x:800,y:320,selected:false},{x:620,y:300,selected:false},{x:770,y:680,selected:false},{x:710,y:500,selected:false}];
         elements = juego1;
         fondo_image = new Image();
         fondo_image.src = '{{ URL::asset('image/fondo.jpg')}}';
